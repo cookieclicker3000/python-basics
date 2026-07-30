9291a0d9c0dc621c5a053f553d66eefacb294ded
Kevin Männik <kevmannik@gmail.com>
2026-07-30 21:15:19 +0300

    Esimene commit - python Basics Algus

---
+++ õpime.py
@@ -0,0 +1,20 @@
+
+#See programm ütleb tere ja küsib mu vanuse kohta.
+
+print("hello, world!")
+
+print("what is your name?")
+
+name = input(">")
+
+print("It is good to meet you, " + name)
+
+print("The lenght of your name is:")
+
+print(len(name))
+
+print("What is your age " + name + "?")
+
+age = input(">")
+
+print("Cool! You will be turning " + str(int(age) + 1) + " in a year.")
