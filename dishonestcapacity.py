99df2630b41fd0b04e87964e19063d6bfe5c7eb7
Kevin Männik <kevmannik@gmail.com>
2026-08-01 15:07:11 +0300

    mahutavuse kadu

--- dishonestcapacity.py
+++ dishonestcapacity.py
@@ -1,18 +1,23 @@
-print('Enter TB or GB for the advertised unit:')
-unit = input('>')
+# arvutus et leida kui palju tegelikult sa mahtu saad kui ostad endale mälupulga vs kui palju reklaamitakse:
+#esiteks küsin mis mahuühiku kasutaja ostis ja arvutan selle mahutavuse kao kordaja
+#teiseks küsin palju mahtu osteti?
 
-# Calculate the amount that the advertised capacity lies:
-if unit == 'TB' or unit == 'tb':
-    discrepancy = 1000000000000 / 1099511627776
-elif unit == 'GB' or unit == 'gb':
-    discrepancy = 1000000000 / 1073741824
+print("Kas ostsite TB ühikut mahtu või GB ühikut mahtu")
+ühik = input(">")
 
-print('Enter the advertised capacity:')
-advertised_capacity = input('>')
-advertised_capacity = float(advertised_capacity)
+if ühik == "GB":
+    Mahutavuse_kadu = 1000000000/1073741824
+elif ühik == "gb":
+    Mahutavuse_kadu = 1000000000/1073741824
+elif ühik == "TB":
+    Mahutavuse_kadu = 1000000000000/1099511627776
+elif ühik == "tb":
+    Mahutavuse_kadu = 1000000000000/1099511627776
 
-# Calculate the real capacity, round it to the nearest hundredths,
-# and convert it to a string so it can be concatenated:
-real_capacity = str(round(advertised_capacity * discrepancy, 2))
+print("Kui palju mahtu te ostsite?")
+ostetud_maht = input(">")
+ostetud_maht = float(ostetud_maht)
 
-print('The actual capacity is ' + real_capacity + ' ' + unit)
+päris_maht = str(round(ostetud_maht * Mahutavuse_kadu, 2))
+
+print("Sulle reklaamiti " + str(ostetud_maht) + ühik + " mahtu, tegelikult said " + päris_maht + ühik)
