4f138f8048026b8aa9e79208d6f7ff83f662e835
Kevin Männik <kevmannik@gmail.com>
2026-08-03 21:40:03 +0300

    .

---
+++ guess_the_number.py
@@ -0,0 +1,30 @@
+
+import random
+
+
+
+print("Terekest! Mõtle mingi numbri peale ja ma arvan selle ära!")
+pakutud_number = input("Mõtlen numbrile...")
+
+
+first_number = 50
+print("Kas su number on " + str(random_number) + "?")
+vastus = input("")
+
+while True:  #MAIN MÄNGU LOOP
+    if vastus == "ei":
+        print("Kas pakkusin liiga (k)õrgele või (m)adalale)?")
+        vastus = input(">")
+        if vastus == "k":
+           
+        
+
+        elif vastus == "m":
+          
+
+    elif vastus == "jah":
+        print("EZ money")
+        break
+
+
+
