6f0b8c7cc0fa78f1634f2b4a26837c6ff3da68dc
Kevin Männik <kevmannik@gmail.com>
2026-08-04 23:42:52 +0300

    numbri arvamine

--- guess_the_number.py
+++ guess_the_number.py
@@ -3,11 +3,14 @@ import random
 
 
 
-print("Terekest! Mõtle mingi numbri peale ja ma arvan selle ära!")
+print("Terekest! Mõtle number 1-100 ja ma arvan selle ära!")
 pakutud_number = input("Mõtlen numbrile...")
 
+min = 1
+max = 100
 
-first_number = 50
+
+random_number = random.randint(1,100)
 print("Kas su number on " + str(random_number) + "?")
 vastus = input("")
 
@@ -15,15 +18,22 @@ while True:  #MAIN MÄNGU LOOP
     if vastus == "ei":
         print("Kas pakkusin liiga (k)õrgele või (m)adalale)?")
         vastus = input(">")
+
         if vastus == "k":
-           
+            max = random_number - 1
+            max = (min + max) / 2
+            print(int(max))
+            if
         
 
         elif vastus == "m":
+            print("jou")
+            
           
 
+
     elif vastus == "jah":
-        print("EZ money")
+        print("Ma ütlesin, et arvan numbri ära.")
         break
 
 
